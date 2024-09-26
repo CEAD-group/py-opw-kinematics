05fe562c6b6bea5946c93308b2a128746e8f216d
Thijs Damsma <thijs@damsma.net>
2024-09-27 00:36:53 +0200

    test euler working

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use nalgebra::{Matrix3, Rotation3, Rotation, Unit, Vector3};
-use pyo3::prelude::*;
+use nalgebra::{Matrix3, Rotation, Rotation3, Unit, Vector3};
 use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
 
 #[pyclass]
 #[derive(Clone)]
@@ -19,20 +19,13 @@ impl EulerConvention {
 
     fn _to_rotation_matrix(&self, angles: [f64; 3]) -> Rotation3<f64> {
         let [a1, a2, a3] = angles;
-        let mut seq = self._seq;
-
+        let r1 = Rotation3::from_axis_angle(&self._seq[0], a1);
+        let r2 = Rotation3::from_axis_angle(&self._seq[1], a2);
+        let r3 = Rotation3::from_axis_angle(&self._seq[2], a3);
         if self.extrinsic {
-            seq.reverse();
-        }
-
-        let r1 = Rotation3::from_axis_angle(&seq[0], a1);
-        let r2 = Rotation3::from_axis_angle(&seq[1], a2);
-        let r3 = Rotation3::from_axis_angle(&seq[2], a3);
-
-        if self.extrinsic {
-            r1 * r2 * r3
-        } else {
             r3 * r2 * r1
+        } else {
+            r1 * r2 * r3
         }
     }
 }
@@ -42,9 +35,10 @@ impl EulerConvention {
     #[new]
     fn new(sequence: String, extrinsic: bool, degrees: bool) -> PyResult<Self> {
         if sequence.len() != 3 {
-            return Err(PyErr::new::<PyValueError, _>(
-                format!("Expected a 3-character sequence, but got {} characters", sequence.len()),
-            ));
+            return Err(PyErr::new::<PyValueError, _>(format!(
+                "Expected a 3-character sequence, but got {} characters",
+                sequence.len()
+            )));
         }
 
         let _seq: [Unit<Vector3<f64>>; 3] = sequence
@@ -53,13 +47,16 @@ impl EulerConvention {
                 'X' => Ok(Unit::new_normalize(Vector3::new(1.0, 0.0, 0.0))),
                 'Y' => Ok(Unit::new_normalize(Vector3::new(0.0, 1.0, 0.0))),
                 'Z' => Ok(Unit::new_normalize(Vector3::new(0.0, 0.0, 1.0))),
-                _ => Err(PyErr::new::<PyValueError, _>(
-                    format!("Invalid character '{}'. Expected only 'X', 'Y', or 'Z'.", c)
-                )),
+                _ => Err(PyErr::new::<PyValueError, _>(format!(
+                    "Invalid character '{}'. Expected only 'X', 'Y', or 'Z'.",
+                    c
+                ))),
             })
             .collect::<Result<Vec<_>, _>>()?
             .try_into()
-            .map_err(|_| PyErr::new::<PyValueError, _>("Invalid sequence. Must be exactly 3 characters."))?;
+            .map_err(|_| {
+                PyErr::new::<PyValueError, _>("Invalid sequence. Must be exactly 3 characters.")
+            })?;
 
         Ok(EulerConvention {
             sequence,
@@ -69,28 +66,43 @@ impl EulerConvention {
         })
     }
 
-    fn convert(&self, other: &EulerConvention, angles: [f64; 3]) -> PyResult<[f64; 3]> {
-        if other.sequence == self.sequence && other.extrinsic == self.extrinsic {
-            return Ok(angles);
-        }
+    fn convert(&self, other: &EulerConvention, mut angles: [f64; 3]) -> PyResult<[f64; 3]> {
+        // Convert from degrees to radians if necessary
         if self.degrees {
             angles = angles.map(|angle| angle.to_radians());
-                };
+        }
+
+        // Perform the conversion using the internal logic (in radians)
         let rot_matrix = self._to_rotation_matrix(angles);
-        let result = other._from_rotation_matrix(rot_matrix);
+        let mut result = other._from_rotation_matrix(rot_matrix);
+
+        // Convert back to degrees if necessary
         if self.degrees {
             result = result.map(|angle| angle.to_degrees());
-
         }
-        Ok(result);
+
+        Ok(result)
     }
 
     fn from_rotation_matrix(&self, rot: [[f64; 3]; 3]) -> [f64; 3] {
         let rotation = Rotation3::from_matrix_unchecked(Matrix3::from(rot));
-        self._from_rotation_matrix(rotation)
+        let mut angles = self._from_rotation_matrix(rotation);
+
+        // Convert back to degrees if necessary
+        if self.degrees {
+            angles = angles.map(|angle| angle.to_degrees());
+        }
+
+        angles
     }
 
-    fn to_rotation_matrix(&self, angles: [f64; 3]) -> [[f64; 3]; 3] {
+    fn to_rotation_matrix(&self, mut angles: [f64; 3]) -> [[f64; 3]; 3] {
+        // Convert from degrees to radians if necessary
+        if self.degrees {
+            angles = angles.map(|angle| angle.to_radians());
+        }
+
+        // Perform the conversion using the internal logic (in radians)
         let matrix = self._to_rotation_matrix(angles).into_inner();
         [
             [matrix[(0, 0)], matrix[(0, 1)], matrix[(0, 2)]],
@@ -99,7 +111,6 @@ impl EulerConvention {
         ]
     }
 }
-
 
 /// Module initialization for Python
 #[pymodule]
---
+++ test_euler.py
@@ -0,0 +1,55 @@
+import pytest
+from py_opw_kinematics import EulerConvention
+import scipy
+import numpy as np
+
+
+@pytest.mark.parametrize("extrinsic", [True, False])
+@pytest.mark.parametrize("seq", ["XYZ", "XZY"])
+@pytest.mark.parametrize("angles", [[30, 0, 50], [60, 40, 90]])
+def test_euler_convention_to_rotation_matrix(extrinsic, seq, angles):
+    # Create EulerConvention object
+    convention = EulerConvention(seq, extrinsic=extrinsic, degrees=True)
+
+    # Get the result from EulerConvention
+    res = convention.to_rotation_matrix(angles)
+
+    # Get the expected result using SciPy
+    expected = scipy.spatial.transform.Rotation.from_euler(
+        seq.lower() if extrinsic else seq.upper(), angles, degrees=True
+    ).as_matrix()
+
+    # Compare the results using np.isclose
+    assert np.all(
+        np.isclose(res, expected)
+    ), f"Failed for {seq=}, {angles=}, {extrinsic=}"
+
+
+@pytest.mark.parametrize("in_extrinsic", [True, False])
+@pytest.mark.parametrize("in_seq", ["XYZ", "XZY"])
+@pytest.mark.parametrize("in_angles", [[30, 0, 50], [60, 40, 90]])
+@pytest.mark.parametrize("out_extrinsic", [True, False])
+@pytest.mark.parametrize("out_seq", ["XYZ", "XZY"])
+def test_euler_convention_to_other_convention(
+    in_extrinsic, in_seq, in_angles, out_extrinsic, out_seq
+):
+    # Create EulerConvention object
+    in_convention = EulerConvention(in_seq, extrinsic=in_extrinsic, degrees=True)
+    out_convention = EulerConvention(out_seq, extrinsic=out_extrinsic, degrees=True)
+
+    # Get the result from EulerConvention
+    res = in_convention.convert(out_convention, in_angles)
+
+    # Get the expected result using SciPy
+    expected = scipy.spatial.transform.Rotation.from_euler(
+        in_seq.lower() if in_extrinsic else in_seq.upper(), in_angles, degrees=True
+    ).as_matrix()
+
+    expected = scipy.spatial.transform.Rotation.from_matrix(expected).as_euler(
+        out_seq.lower() if out_extrinsic else out_seq.upper(), degrees=True
+    )
+
+    # Compare the results using np.isclose
+    assert np.all(
+        np.isclose(res, expected)
+    ), f"Failed for {in_seq=}, {in_angles=}, {in_extrinsic=}, {out_seq=}, {out_extrinsic=}"
